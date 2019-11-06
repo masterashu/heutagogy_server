95d537479974aa4f5dc1ac038354285e4efdee8f
Ashutosh Chauhan <masterashu@live.in>
2019-11-06 21:05:17 +0530

    setting production

--- heutagogy_server/settings.py
+++ heutagogy_server/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'l+9akmif#_7eb%b07rm66m&rx=x!fv&d@l)^ejoe03o_y#x*9&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-        '*',
+    '*',
 ]
 
 
