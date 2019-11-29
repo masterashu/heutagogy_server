af62591a0333c2e88ca3bf333f940d470dce1ad1
Ashutosh Chauhan <masterashu@live.in>
2019-11-30 00:07:33 +0530

    fix context passing to view

--- evaluation/views.py
+++ evaluation/views.py
@@ -13,7 +13,8 @@ from .serializers import LessonSerializer
 class LessonsGetView(APIView):
     def get(self, request):
         lessons = Lesson.objects.filter(type='L')
-        serializer = LessonSerializer(lessons, many=True, context={'request': request})
+        serializer = LessonSerializer(
+            lessons, many=True, context={'request': request})
         return Response(serializer.data)
 
 
@@ -28,14 +29,16 @@ class LessonDetailView(APIView):
 
 
 def download_file(request, file_name):
-    file_path = settings.MEDIA_ROOT +'/'+ file_name
+    file_path = settings.MEDIA_ROOT + '/' + file_name
     file_wrapper = FileWrapper(open(file_path, 'rb'))
     file_mimetype = mimetypes.guess_type(file_path)
     response = HttpResponse(file_wrapper, content_type=file_mimetype)
     response['X-Sendfile'] = file_path
     response['Content-Length'] = os.stat(file_path).st_size
-    response['Content-Disposition'] = 'attachment; filename=%s' % smart_str(file_name)
+    response['Content-Disposition'] = 'attachment; filename=%s' % smart_str(
+        file_name)
     return response
+
 
 class AssessmentDetailView(APIView):
     def get(self, request):
@@ -43,5 +46,5 @@ class AssessmentDetailView(APIView):
             assessment = Lesson.objects.filter(type='A')[0]
         except IndexError:
             return Response(status=404)
-        serializer = LessonSerializer(assessment)
-        return Response(serializer.data)
+        serializer = LessonSerializer(assessment, context={'request': request})
+        return Response(serializer.data, )
